c316487b9c17446fc4096f87ebfe6b83874f3e03
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-08-31 17:18:02 -0300

    feat: Creating base ValueObject and Address

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/ValueObjects/Address.cs
@@ -0,0 +1,34 @@
+﻿namespace MedicalCenterManagement.Domain.ValueObjects;
+
+public class Address : ValueObject
+{
+    public string PublicArea { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+    public string ZipCode { get; set; }
+
+    public Address(
+        string publicArea,
+        string city,
+        string state,
+        string country,
+        string zipCode
+    )
+    {
+        PublicArea = publicArea;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipCode;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return PublicArea;
+        yield return City;
+        yield return State;
+        yield return Country;
+        yield return ZipCode;
+    }
+}
