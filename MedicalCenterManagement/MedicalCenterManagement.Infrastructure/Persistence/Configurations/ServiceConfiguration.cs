c263082a268a04d76a7b225f492587441b3fb2fe
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-02 19:58:38 -0300

    feat: Adding configurations

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Persistence/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Persistence.Configurations;
+
+public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+{
+    public void Configure(EntityTypeBuilder<Service> builder)
+    {
+        builder
+            .ToTable("Services");
+        
+        builder
+            .HasKey(s => s.Id);
+
+        builder
+            .HasIndex(s => s.Id);
+    
+        builder
+            .Property(s => s.Name)
+            .HasMaxLength(Patient.NameMaxLength)
+            .IsRequired();
+        
+        builder
+            .Property(s => s.Description)
+            .HasMaxLength(Service.DescriptionMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(s => s.Price)
+            .IsRequired();
+
+        builder
+            .Property(s => s.Period)
+            .IsRequired();
+        
+        builder
+            .Property(s => s.IsDeleted)
+            .HasDefaultValue(false)
+            .IsRequired();
+    }
+}
