b6e6721e74c9792497e4c825589db4a489a55e07
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-02 20:00:16 -0300

    feat: Creating ServiceCollectionExtensions in Infrastructure layer

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<MedicalCenterManagementDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(connectionString)));
+        services.AddScoped<IMedicalCenterManagementDbContext>(provider => provider.GetRequiredService<MedicalCenterManagementDbContext>());
+        
+        return services;
+    }
+}
