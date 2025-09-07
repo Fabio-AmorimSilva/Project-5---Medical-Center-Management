a39aa4672ee38740ea3a761dacd2c75ddb0e6993
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-07 17:52:24 -0300

    feat: Creating new class for erro messages

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Messages/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Domain.Messages;
+
+public static class ErrorMessages
+{
+    public static string CannotBeEmpty(string field)
+        => $"{field} cannot be empty";
+
+    public static string HasMaxLength(string field, int length)
+        => $"{field} must have a maximum of {length} characters";
+
+    public static string NotFound(string field)
+        => $"{field} cannot be found";
+}
