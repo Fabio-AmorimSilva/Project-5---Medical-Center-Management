28e99595d1ccc27cb35a6d722a1dd796b7604ca8
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-07 16:19:15 -0300

    feat: Creating mediator class and implementing publish method

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Common/Mediator.cs
@@ -0,0 +1,15 @@
+﻿namespace MedicalCenterManagement.Application.Common;
+
+public class Mediator(IServiceProvider serviceProvider) : IMediator
+{
+    public async Task<TResponse> Publish<TResponse>(IRequest<TResponse> request)
+    {
+        var handlerType = typeof(IHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+        dynamic? handler = serviceProvider.GetService(handlerType);
+        
+        if(handler is null)
+            throw new Exception($"Handler {handlerType} not found");
+        
+        return await handler.Handle(request);
+    }
+}
