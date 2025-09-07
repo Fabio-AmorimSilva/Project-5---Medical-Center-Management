d3fdd68bff2d81eb34a40a294ec5bf43e8c6ce0a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-07 16:20:33 -0300

    feat: Creating ServiceCollectionExtensions for Application layer and implementing AddMediator method

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediator();
+
+        return services;
+    }
+
+    private static void AddMediator(this IServiceCollection services, params Assembly[]? assemblies)
+    {
+        if (assemblies is null || assemblies.Length == 0)
+            assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        services.AddSingleton<IMediator, Mediator>();
+
+        var types = assemblies.SelectMany(a => a.GetTypes());
+
+        foreach (var type in types)
+        {
+            var interfaces = type.GetInterfaces();
+
+            foreach (var @interface in interfaces)
+            {
+                if (@interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IHandler<,>))
+                    services.AddTransient(@interface, type);
+            }
+        }
+    }
+}
