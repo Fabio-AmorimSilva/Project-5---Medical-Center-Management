d4e9113cb9bb063cf289ae559f474d9fd7f14609
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-07 17:51:37 -0300

    feat: Creating Response model for standardization of all responses in application layer

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Models/Response.cs
@@ -0,0 +1,43 @@
+﻿namespace MedicalCenterManagement.Application.Models;
+
+public class Response
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+
+    protected Response()
+    {
+    }
+
+    public static Response Success()
+        => new()
+        {
+            IsSuccess = true
+        };
+
+    public static Response Error(string message)
+        => new()
+        {
+            IsSuccess = false,
+            Message = message
+        };
+}
+
+public class Response<T> : Response
+{
+    public T? Data { get; set; }
+
+    public static Response<T> Success(T data)
+        => new()
+        {
+            IsSuccess = true,
+            Data = data
+        };
+
+    public new static Response<T> Error(string message)
+        => new()
+        {
+            IsSuccess = false,
+            Message = message
+        };
+}
