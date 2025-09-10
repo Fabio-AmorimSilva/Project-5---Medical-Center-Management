a2b653a0da73fe0ea751f6f3b52a8759af449c3a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-10 19:51:30 -0300

    feat: Implementing UpdateDoctorCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Commands/UpdateDoctor/UpdateDoctorCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace MedicalCenterManagement.Application.Doctors.Commands.UpdateDoctor;
+
+public record UpdateDoctorCommand(
+    Guid DoctorId,
+    Speciality Speciality,
+    string Crm
+) : IRequest<Response>;
+
+public class UpdateDoctorCommandValidator : AbstractValidator<UpdateDoctorCommand>
+{
+    public UpdateDoctorCommandValidator()
+    {
+        RuleFor(command => command.DoctorId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateDoctorCommand.DoctorId)));
+
+        RuleFor(command => command.Speciality)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateDoctorCommand.Speciality)));
+
+        RuleFor(command => command.Crm)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateDoctorCommand.Crm)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Commands/UpdateDoctor/UpdateDoctorCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace MedicalCenterManagement.Application.Doctors.Commands.UpdateDoctor;
+
+public class UpdateDoctorCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<UpdateDoctorCommand, Response>
+{
+    public async Task<Response> Handle(UpdateDoctorCommand request)
+    {
+        var doctor = await context.Doctors.FirstOrDefaultAsync(d => d.Id == request.DoctorId);
+
+        if (doctor is null)
+            return new NotFoundResponse<Doctor>(ErrorMessages.NotFound<Doctor>());
+
+        doctor.Update(
+            speciality: request.Speciality,
+            crm: request.Crm
+        );
+
+        var crmAlreadyExists = await context.Doctors
+            .WithSpecification(new CrmAlreadyExists(doctor.Id, doctor.Crm))
+            .AnyAsync();
+
+        if (crmAlreadyExists)
+            return new UnprocessableResponse<Guid>(ErrorMessages.MustBeUnique(nameof(doctor.Crm)));
+
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
