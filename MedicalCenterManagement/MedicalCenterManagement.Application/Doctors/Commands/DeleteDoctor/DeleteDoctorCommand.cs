6c87e30c186ede4219784178025280e6bbf4ba53
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-11 19:46:51 -0300

    feat: Adding DeleteDoctorCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Commands/DeleteDoctor/DeleteDoctorCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Application.Doctors.Commands.DeleteDoctor;
+
+public record DeleteDoctorCommand(Guid DoctorId) : IRequest<Response>;
+
+public class DeleteDoctorCommandValidator : AbstractValidator<DeleteDoctorCommand>
+{
+    public DeleteDoctorCommandValidator()
+    {
+        RuleFor(command => command.DoctorId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteDoctorCommand.DoctorId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Commands/DeleteDoctor/DeleteDoctorCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MedicalCenterManagement.Application.Doctors.Commands.DeleteDoctor;
+
+public class DeleteDoctorCommandHandler(IMedicalCenterManagementDbContext context) : IHandler<DeleteDoctorCommand, Response>
+{
+    public async Task<Response> Handle(DeleteDoctorCommand request)
+    {
+        var doctor = await context.Doctors.FirstOrDefaultAsync(d => d.Id == request.DoctorId);
+
+        if (doctor is null)
+            return new NotFoundResponse<Doctor>(ErrorMessages.NotFound<Doctor>());
+
+        context.Doctors.Remove(doctor);
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
