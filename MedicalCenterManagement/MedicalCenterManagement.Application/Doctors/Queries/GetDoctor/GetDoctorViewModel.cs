c17011f998eb529e8ee8c8cd7ae4a2d606986052
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-12 20:09:42 -0300

    refactor: Updating GetDoctorViewModel

--- MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Queries/GetDoctor/GetDoctorViewModel.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Doctors/Queries/GetDoctor/GetDoctorViewModel.cs
@@ -2,8 +2,8 @@
 
 public record GetDoctorViewModel
 {
-    public string Name { get; init; }
-    public string LastName { get; set; }
+    public string Name { get; init; } = null!;
+    public string LastName { get; set; } = null!;
     public Speciality Speciality { get; set; }
-    public string Crm { get; set; }
+    public string Crm { get; set; } = null!;
 }
