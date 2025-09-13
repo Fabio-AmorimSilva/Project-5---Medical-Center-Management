c449f2a6e8e5b1289480c2db067167a6a0d2e7b1
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-13 15:18:21 -0300

    feat: Creating new payload

---
+++ MedicalCenterManagement/MedicalCenterManagement.Api/Payloads/Doctors/UpdateDoctorPayload.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicalCenterManagement.Api.Payloads.Doctors;
+
+public record UpdateDoctorPayload(
+    Speciality Speciality,
+    string Crm
+)
+{
+    public UpdateDoctorCommand AsCommand(Guid doctorId)
+        => new(
+            DoctorId: doctorId,
+            Speciality: Speciality,
+            Crm: Crm
+        );
+}
