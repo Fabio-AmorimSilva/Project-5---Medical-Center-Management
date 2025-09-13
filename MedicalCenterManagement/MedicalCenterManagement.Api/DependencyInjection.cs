9bf2fa1f536af5ed9b66f2e7394919e84d2f7317
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-13 16:19:12 -0300

    feat: Adding new DependencyInjection extension methods

---
+++ MedicalCenterManagement/MedicalCenterManagement.Api/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services
+            .AddGlobalExceptionHandler()
+            .AddProblemDetailsService();
+        
+        return services;
+    }
+
+    private static void AddProblemDetailsService(this IServiceCollection services)
+    {
+        services.AddProblemDetails(configure =>
+        {
+            configure.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+            };
+        });
+    }
+
+    private static IServiceCollection AddGlobalExceptionHandler(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        
+        return services;
+    }
+}
