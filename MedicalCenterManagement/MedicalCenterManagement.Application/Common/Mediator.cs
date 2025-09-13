924400700a3c4824aa6b1dd54fe71a1ad8daa8df
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-13 15:17:55 -0300

    refactor: Updating mediator method

--- MedicalCenterManagement/MedicalCenterManagement.Application/Common/Mediator.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Common/Mediator.cs
@@ -1,15 +1,30 @@
 ﻿namespace MedicalCenterManagement.Application.Common;
 
-public class Mediator(IServiceProvider serviceProvider) : IMediator
+public class Mediator(IServiceScopeFactory scopeFactory) : IMediator
 {
     public async Task<TResponse> Publish<TResponse>(IRequest<TResponse> request)
     {
-        var handlerType = typeof(IHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
-        dynamic? handler = serviceProvider.GetService(handlerType);
-        
-        if(handler is null)
+        using var scope = scopeFactory.CreateScope();
+
+        var handlerType = typeof(IHandler<,>)
+            .MakeGenericType(request.GetType(), typeof(TResponse));
+
+        var handler = scope.ServiceProvider.GetService(handlerType);
+        if (handler is null)
             throw new Exception($"Handler {handlerType} not found");
-        
-        return await handler.Handle(request);
+
+        var method = handlerType.GetMethod("Handle");
+        if (method is null)
+            throw new Exception($"Handler {handlerType} does not have a Handle method");
+
+        var task = (Task)method.Invoke(handler, [request])!;
+
+        await task.ConfigureAwait(false);
+
+        var resultProperty = task.GetType().GetProperty("Result");
+        if (resultProperty is null)
+            throw new Exception("Handle did not return a result");
+
+        return (TResponse)resultProperty.GetValue(task)!;
     }
 }
