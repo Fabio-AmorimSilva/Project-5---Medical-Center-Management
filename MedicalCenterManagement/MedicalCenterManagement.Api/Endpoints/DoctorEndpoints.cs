d0a4f9beccfa0e476ede2c58a4889bccd5d41e43
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-13 15:19:02 -0300

    feat: Adding Endpoints for Doctor

---
+++ MedicalCenterManagement/MedicalCenterManagement.Api/Endpoints/DoctorEndpoints.cs
@@ -0,0 +1,56 @@
+﻿namespace MedicalCenterManagement.Api.Endpoints;
+
+public class DoctorEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        const string url = "/api/doctors";
+
+        var mapGroup = app.MapGroup(url);
+
+        mapGroup.MapPost("/",
+            [ProducesResponseType(typeof(Response<Guid>), StatusCodes.Status200OK)]
+            async ([FromServices] IMediator mediator, [FromBody] CreateDoctorCommand command) =>
+            {
+                var response = await mediator.Publish(command);
+
+                return Results.Created(url, response);
+            });
+
+        mapGroup.MapGet("/",
+            [ProducesResponseType(typeof(Response<IEnumerable<ListDoctorViewModel>>), StatusCodes.Status200OK)]
+            async ([FromServices] IMediator mediator) =>
+            {
+                var response = await mediator.Publish(new ListDoctorsQuery());
+
+                return Results.Ok(response);
+            });
+
+        mapGroup.MapGet("/{doctorId:guid}",
+            [ProducesResponseType(typeof(Response<GetDoctorViewModel>), StatusCodes.Status200OK)]
+            async ([FromRoute] Guid doctorId, [FromServices] IMediator mediator) =>
+            {
+                var response = await mediator.Publish(new GetDoctorQuery(doctorId));
+
+                return Results.Ok(response);
+            });
+
+        mapGroup.MapPut("/{doctorId:guid}",
+            [ProducesResponseType(typeof(Response), StatusCodes.Status204NoContent)]
+            async ([FromRoute] Guid doctorId, [FromServices] IMediator mediator, [FromBody] UpdateDoctorPayload payload) =>
+            {
+                await mediator.Publish(payload.AsCommand(doctorId));
+
+                return Results.NoContent();
+            });
+
+        mapGroup.MapDelete("/{doctorId:guid}",
+            [ProducesResponseType(typeof(Response), StatusCodes.Status204NoContent)]
+            async ([FromRoute] Guid doctorId, [FromServices] IMediator mediator) =>
+            {
+                await mediator.Publish(new DeleteDoctorCommand(doctorId));
+
+                return Results.NoContent();
+            });
+    }
+}
