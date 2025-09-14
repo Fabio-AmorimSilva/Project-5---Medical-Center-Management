5a87ef59efebd299c426c2f5e149f659b5ec1163
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-14 17:13:38 -0300

    feat: Adding CreatePatientCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Patients/Commands/CreatePatient/CreatePatientCommand.cs
@@ -0,0 +1,67 @@
+﻿namespace MedicalCenterManagement.Application.Patients.Commands.CreatePatient;
+
+public record CreatePatientCommand(
+    string Name,
+    string LastName,
+    DateTime Birth,
+    string PhoneNumber,
+    string Email,
+    string Cpf,
+    BloodType BloodType,
+    AddressDto Address,
+    decimal Height,
+    decimal Weight
+) : IRequest<Response<Guid>>;
+
+public class CreatePatientCommandValidator : AbstractValidator<CreatePatientCommand>
+{
+    public CreatePatientCommandValidator()
+    {
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Name)))
+            .MaximumLength(Person.NameMaxLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(CreatePatientCommand.Name), Person.NameMaxLength));
+
+        RuleFor(command => command.LastName)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Name)))
+            .MaximumLength(Person.LastNameMaxLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(CreatePatientCommand.Name), Person.LastNameMaxLength));
+
+        RuleFor(command => command.Birth)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Birth)));
+
+        RuleFor(command => command.PhoneNumber)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.PhoneNumber)))
+            .Matches(PhoneValidation.PhoneNumberRegex)
+            .WithMessage(ErrorMessages.NotFound(nameof(CreatePatientCommand.PhoneNumber)));
+
+        RuleFor(command => command.Email)
+            .EmailAddress()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Email)));
+
+        RuleFor(command => command.Cpf)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Cpf)))
+            .Matches(CpfValidation.CpfRegex)
+            .WithMessage(ErrorMessages.NotFound(nameof(CreatePatientCommand.Cpf)));
+
+        RuleFor(command => command.BloodType)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.BloodType)));
+
+        RuleFor(command => command.Address)
+            .SetValidator(new AddressDtoValidator());
+        
+        RuleFor(command => command.Weight)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Weight)));
+        
+        RuleFor(command => command.Height)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreatePatientCommand.Height)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Patients/Commands/CreatePatient/CreatePatientCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿namespace MedicalCenterManagement.Application.Patients.Commands.CreatePatient;
+
+public class CreatePatientCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<CreatePatientCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(CreatePatientCommand request)
+    {
+        var patient = new Patient(
+            name: request.Name,
+            lastName: request.LastName,
+            birth: request.Birth,
+            phoneNumber: request.PhoneNumber,
+            email: request.Email,
+            cpf: request.Cpf,
+            bloodType: request.BloodType,
+            address: new Address(
+                publicArea: request.Address.PublicArea,
+                city: request.Address.City,
+                country: request.Address.Country,
+                state: request.Address.State,
+                zipCode: request.Address.ZipCode
+            ),
+            weight: request.Weight,
+            height: request.Height
+        );
+
+        var cpfAlreadyExists = await context.Persons
+            .WithSpecification(new CpfAlreadyExists(patient.Cpf))
+            .AnyAsync();
+
+        if (cpfAlreadyExists)
+            return new UnprocessableResponse<Guid>(ErrorMessages.MustBeUnique(nameof(patient.Cpf)));
+
+        var emailAlreadyExists = await context.Persons
+            .WithSpecification(new EmailAlreadyExists(patient.Id, patient.Email))
+            .AnyAsync();
+
+        if (emailAlreadyExists)
+            return new UnprocessableResponse<Guid>(ErrorMessages.MustBeUnique(nameof(patient.Email)));
+
+        await context.Patients.AddAsync(patient);
+        await context.SaveChangesAsync();
+
+        return new CreatedResponse<Guid>(patient.Id);
+    }
+}
