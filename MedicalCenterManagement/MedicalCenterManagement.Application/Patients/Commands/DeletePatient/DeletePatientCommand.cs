1c16ca3a1d09e40ad7088fd5a5eaf1ba4528be0f
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-14 17:13:54 -0300

    feat: Adding DeletePatientCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Patients/Commands/DeletePatient/DeletePatientCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Application.Patients.Commands.DeletePatient;
+
+public record DeletePatientCommand(Guid PatientId) : IRequest<Response>;
+
+public class DeletePatientCommandValidator : AbstractValidator<DeletePatientCommand>
+{
+    public DeletePatientCommandValidator()
+    {
+        RuleFor(command => command.PatientId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeletePatientCommand.PatientId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Patients/Commands/DeletePatient/DeletePatientCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MedicalCenterManagement.Application.Patients.Commands.DeletePatient;
+
+public class DeletePatientCommandHandler(IMedicalCenterManagementDbContext context) : IHandler<DeletePatientCommand, Response>
+{
+    public async Task<Response> Handle(DeletePatientCommand request)
+    {
+        var patient = await context.Patients.FirstOrDefaultAsync(p => p.Id == request.PatientId);
+
+        if (patient is null)
+            return new NotFoundResponse<Patient>(ErrorMessages.NotFound<Patient>());
+
+        context.Patients.Remove(patient);
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
