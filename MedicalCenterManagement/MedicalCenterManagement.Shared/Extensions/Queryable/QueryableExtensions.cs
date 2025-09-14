7621e38fe82105160f1a3cf834f55fa23461182d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-14 16:49:02 -0300

    feat: Creating new project and extension method for IQueryables

---
+++ MedicalCenterManagement/MedicalCenterManagement.Shared/Extensions/Queryable/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Linq.Expressions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> WhereIf<T>(
+        this IQueryable<T> query,
+        bool condition,
+        Expression<Func<T, bool>> predicate
+    ) => condition
+        ? query.Where(predicate)
+        : query;
+}
