193beaae43938c70d7a00d87b9c35b0d456c9144
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-14 17:18:05 -0300

    feat: Adding Update method to Patient entity

--- MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/Patient.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/Patient.cs
@@ -4,7 +4,6 @@ public class Patient : Person, IHasIsDeleted
 {
     public decimal Height { get; private set; }
     public decimal Weight { get; private set; }
-    public Address Address { get; private set; }
     public bool IsDeleted { get; set; }
     public DateTime? DeletedAt { get; set; }
 
@@ -30,6 +29,17 @@ public class Patient : Person, IHasIsDeleted
         
         Height = height;
         Weight = weight;
-        Address = address;
+    }
+
+    public void Update(
+        decimal height,
+        decimal weight
+    )
+    {
+        Guard.IsNotDefault(height);
+        Guard.IsNotDefault(weight);
+        
+        Height = height;
+        Weight = weight;
     }
 }
