fe9a96af1f00f26e7c42e8c5a4285d0703744462
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-15 19:53:55 -0300

    feat: Creating DeleteServiceCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Services/Commands/DeleteService/DeleteServiceCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Application.Services.Commands.DeleteService;
+
+public record DeleteServiceCommand(Guid ServiceId) : IRequest<Response>;
+
+public class DeleteServiceCommandValidator : AbstractValidator<DeleteServiceCommand>
+{
+    public DeleteServiceCommandValidator()
+    {
+        RuleFor(command => command.ServiceId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteServiceCommand.ServiceId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Services/Commands/DeleteService/DeleteServiceCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace MedicalCenterManagement.Application.Services.Commands.DeleteService;
+
+public class DeleteServiceCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<DeleteServiceCommand, Response>
+{
+    public async Task<Response> Handle(DeleteServiceCommand request)
+    {
+        var service = await context.Services.FirstOrDefaultAsync(s => s.Id == request.ServiceId);
+
+        if (service is null)
+            return new NotFoundResponse<Service>(ErrorMessages.NotFound<Service>());
+
+        context.Services.Remove(service);
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
