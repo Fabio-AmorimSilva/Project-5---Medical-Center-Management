ecac1e6b317604c574b7851d79dc058b67b18e41
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-15 19:55:41 -0300

    feat: Adding new specification for service names

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Specifications/ServiceAlreadyExistsSpec.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalCenterManagement.Domain.Specifications;
+
+public sealed class ServiceAlreadyExistsSpec : Specification<Service>
+{
+    public ServiceAlreadyExistsSpec(Guid serviceId, string name)
+    {
+        Query.Where(s =>
+            s.Id != serviceId &&
+            s.Name == name
+        );
+    }
+}
