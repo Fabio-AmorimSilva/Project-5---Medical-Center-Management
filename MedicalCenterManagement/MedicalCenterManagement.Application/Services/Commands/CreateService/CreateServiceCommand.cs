11231fc2973399e7f7ca8b607182cbc09156056b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-15 19:54:44 -0300

    feat: Creating CreateServiceCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Services/Commands/CreateService/CreateServiceCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace MedicalCenterManagement.Application.Services.Commands.CreateService;
+
+public record CreateServiceCommand(
+    string Name,
+    string Description,
+    decimal Price,
+    int Period
+) : IRequest<Response<Guid>>;
+
+public class CreateServiceCommandValidator : AbstractValidator<CreateServiceCommand>
+{
+    public CreateServiceCommandValidator()
+    {
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateServiceCommand.Name)))
+            .MaximumLength(Service.NameMaxLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(CreateServiceCommand.Name), Service.NameMaxLength));
+
+        RuleFor(command => command.Description)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateServiceCommand.Description)))
+            .MaximumLength(Service.DescriptionMaxLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(CreateServiceCommand.Description), Service.DescriptionMaxLength));
+
+        RuleFor(command => command.Price)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateServiceCommand.Price)));
+
+        RuleFor(command => command.Period)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateServiceCommand.Period)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Services/Commands/CreateService/CreateServiceCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace MedicalCenterManagement.Application.Services.Commands.CreateService;
+
+public class CreateServiceCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<CreateServiceCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(CreateServiceCommand request)
+    {
+        var service = new Service(
+            name: request.Name,
+            description: request.Description,
+            price: request.Price,
+            period: request.Period
+        );
+
+        var serviceAlreadyExists = await context.Services
+            .WithSpecification(new ServiceAlreadyExistsSpec(service.Id, service.Name))
+            .AnyAsync();
+
+        if (serviceAlreadyExists)
+            return new UnprocessableResponse<Guid>(ErrorMessages.MustBeUnique(nameof(service.Name)));
+
+        await context.Services.AddAsync(service);
+        await context.SaveChangesAsync();
+
+        return new CreatedResponse<Guid>(service.Id);
+    }
+}
