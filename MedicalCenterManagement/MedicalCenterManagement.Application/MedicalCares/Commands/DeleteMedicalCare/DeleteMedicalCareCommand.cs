4dd89ba95fe1c1220ccc55102ffaa663979ee250
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-17 19:53:56 -0300

    feat: Creating DeleteMedicalCareCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/DeleteMedicalCare/DeleteMedicalCareCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.DeleteMedicalCare;
+
+public record DeleteMedicalCareCommand(Guid MedicalCareId) : IRequest<Response>;
+
+public class DeleteMedicalCareCommandValidator : AbstractValidator<DeleteMedicalCareCommand>
+{
+    public DeleteMedicalCareCommandValidator()
+    {
+        RuleFor(command => command.MedicalCareId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteMedicalCareCommand.MedicalCareId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/DeleteMedicalCare/DeleteMedicalCareCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.DeleteMedicalCare;
+
+public class DeleteMedicalCareCommandHandler(IMedicalCenterManagementDbContext context) : IHandler<DeleteMedicalCareCommand, Response>
+{
+    public async Task<Response> Handle(DeleteMedicalCareCommand request)
+    {
+        var medicalCare = await context.MedicalCares.FirstOrDefaultAsync(m => m.Id == request.MedicalCareId);
+        
+        if (medicalCare is null)
+            return new NotFoundResponse<MedicalCare>(ErrorMessages.NotFound<MedicalCare>());
+        
+        context.MedicalCares.Remove(medicalCare);
+        await context.SaveChangesAsync();
+        
+        return new NoContentResponse();
+    }
+}
