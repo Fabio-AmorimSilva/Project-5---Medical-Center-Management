cc7813c27c32535590f179d7178bdf52e6e56b7d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-17 19:54:11 -0300

    feat: Creating UpdateMedicalCareCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/UpdateMedicalCare/UpdateMedicalCareCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.UpdateMedicalCare;
+
+public record UpdateMedicalCareCommand(
+    Guid MedicalCareId,
+    string Insurance,
+    DateTime Start,
+    DateTime End,
+    TypeOfService TypeOfService,
+    Guid DoctorId,
+    Guid PatientId,
+    Guid ServiceId
+) : IRequest<Response>;
+
+public class UpdateMedicalCareCommandValidator : AbstractValidator<UpdateMedicalCareCommand>
+{
+    public UpdateMedicalCareCommandValidator()
+    {
+        RuleFor(command => command.MedicalCareId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.MedicalCareId)));
+
+        RuleFor(command => command.Insurance)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.Insurance)));
+
+        RuleFor(command => command.Start)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.Start)));
+
+        RuleFor(command => command.End)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.End)));
+
+        RuleFor(command => command.TypeOfService)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.TypeOfService)));
+
+        RuleFor(command => command.DoctorId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.DoctorId)));
+
+        RuleFor(command => command.PatientId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.PatientId)));
+
+        RuleFor(command => command.ServiceId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateMedicalCareCommand.ServiceId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/UpdateMedicalCare/UpdateMedicalCareCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.UpdateMedicalCare;
+
+public class UpdateMedicalCareCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<UpdateMedicalCareCommand, Response>
+{
+    public async Task<Response> Handle(UpdateMedicalCareCommand request)
+    {
+        var medicalCare = await context.MedicalCares.FirstOrDefaultAsync(mc => mc.Id == request.MedicalCareId);
+
+        if (medicalCare is null)
+            return new NotFoundResponse<MedicalCare>(ErrorMessages.NotFound<MedicalCare>());
+
+        var service = await context.Services.FirstOrDefaultAsync(s => s.Id == request.ServiceId);
+
+        if (service is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Service>());
+
+        var doctor = await context.Doctors.FirstOrDefaultAsync(d => d.Id == request.DoctorId);
+
+        if (doctor is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Doctor>());
+
+        var patient = await context.Patients.FirstOrDefaultAsync(p => p.Id == request.PatientId);
+
+        if (patient is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Service>());
+
+        medicalCare.Update(
+            insurance: request.Insurance,
+            start: request.Start,
+            end: request.End,
+            typeOfService: request.TypeOfService,
+            patient: patient,
+            doctor: doctor,
+            service: service
+        );
+
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
