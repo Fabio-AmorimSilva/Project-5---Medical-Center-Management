c961cee437e6b20ff87a967294840a08ee175c7c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-17 19:53:45 -0300

    feat: Adding CreateMedicalCareCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/CreateMedicalCare/CreateMedicalCareCommand.cs
@@ -0,0 +1,45 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.CreateMedicalCare;
+
+public record CreateMedicalCareCommand(
+    string Insurance, 
+    DateTime Start, 
+    DateTime End, 
+    TypeOfService TypeOfService,
+    Guid DoctorId, 
+    Guid PatientId,
+    Guid ServiceId
+) : IRequest<Response<Guid>>;
+
+public class CreateMedicalCareCommandValidator : AbstractValidator<CreateMedicalCareCommand>
+{
+    public CreateMedicalCareCommandValidator()
+    {
+        RuleFor(command => command.Insurance)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.Insurance)));
+        
+        RuleFor(command => command.Start)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.Start)));
+        
+        RuleFor(command => command.End)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.End)));
+        
+        RuleFor(command => command.TypeOfService)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.TypeOfService)));
+        
+        RuleFor(command => command.DoctorId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.DoctorId)));
+        
+        RuleFor(command => command.PatientId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.PatientId)));
+        
+        RuleFor(command => command.ServiceId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateMedicalCareCommand.ServiceId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Commands/CreateMedicalCare/CreateMedicalCareCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Commands.CreateMedicalCare;
+
+public class CreateMedicalCareCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<CreateMedicalCareCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(CreateMedicalCareCommand request)
+    {
+        var service = await context.Services.FirstOrDefaultAsync(s => s.Id == request.ServiceId);
+
+        if (service is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Service>());
+
+        var doctor = await context.Doctors.FirstOrDefaultAsync(d => d.Id == request.DoctorId);
+
+        if (doctor is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Doctor>());
+
+        var patient = await context.Patients.FirstOrDefaultAsync(p => p.Id == request.PatientId);
+
+        if (patient is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Service>());
+
+        var medicalCare = new MedicalCare(
+            insurance: request.Insurance,
+            start: request.Start,
+            end: request.End,
+            typeOfService: request.TypeOfService,
+            patient: patient,
+            doctor: doctor,
+            service: service
+        );
+
+        await context.MedicalCares.AddAsync(medicalCare);
+        await context.SaveChangesAsync();
+
+        return new CreatedResponse<Guid>(medicalCare.Id);
+    }
+}
