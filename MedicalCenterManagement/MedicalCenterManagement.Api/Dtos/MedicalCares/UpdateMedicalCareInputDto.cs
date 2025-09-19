3698087e9e74830a526d584e1fba17fc3ee98f63
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-19 19:56:48 -0300

    feat: Creating UpdateMedicalCareInputDto

---
+++ MedicalCenterManagement/MedicalCenterManagement.Api/Dtos/MedicalCares/UpdateMedicalCareInputDto.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalCenterManagement.Api.Dtos.MedicalCares;
+
+public record UpdateMedicalCareInputDto(
+    string Insurance,
+    DateTime Start,
+    DateTime End,
+    TypeOfService TypeOfService,
+    Guid DoctorId,
+    Guid PatientId,
+    Guid ServiceId
+)
+{
+    public UpdateMedicalCareCommand AsCommand(Guid medicalCareId)
+        => new(
+            MedicalCareId: medicalCareId,
+            Insurance: Insurance,
+            Start: Start,
+            End: End,
+            TypeOfService: TypeOfService,
+            DoctorId: DoctorId,
+            PatientId: PatientId,
+            ServiceId: ServiceId
+        );
+}
