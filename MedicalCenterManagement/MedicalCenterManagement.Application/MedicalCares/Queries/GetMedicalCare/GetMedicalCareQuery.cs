fe194a9cd93cbf4e613599eed9d6325363579c9e
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-19 19:55:29 -0300

    feat: Creating GetMedicalCareQuery and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Queries/GetMedicalCare/GetMedicalCareQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Queries.GetMedicalCare;
+
+public record GetMedicalCareQuery(Guid  MedicalCareId) : IRequest<Response<GetMedicalCareResponseDto>>;
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/MedicalCares/Queries/GetMedicalCare/GetMedicalCareQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace MedicalCenterManagement.Application.MedicalCares.Queries.GetMedicalCare;
+
+public class GetMedicalCareQueryHandler(IMedicalCenterManagementDbContext context) : IHandler<GetMedicalCareQuery, Response<GetMedicalCareResponseDto>>
+{
+    public async Task<Response<GetMedicalCareResponseDto>> Handle(GetMedicalCareQuery request)
+    {
+        var medicalCare = await context.MedicalCares
+            .AsNoTracking()
+            .FirstOrDefaultAsync(mc => mc.Id == request.MedicalCareId);
+
+        if (medicalCare is null)
+            return new NotFoundResponse<GetMedicalCareResponseDto>(ErrorMessages.NotFound<MedicalCare>());
+
+        return new OkResponse<GetMedicalCareResponseDto>(new GetMedicalCareResponseDto
+        {
+            Insurance = medicalCare.Insurance,
+            Start = medicalCare.Start,
+            End = medicalCare.End,
+            TypeOfService = medicalCare.TypeOfService,
+            Patient = medicalCare.Patient.GetFullName(),
+            Doctor = medicalCare.Doctor.GetFullName(),
+            Service = medicalCare.Service.Name
+        });
+    }
+}
