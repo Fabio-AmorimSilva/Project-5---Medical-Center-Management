9672331b3f345f8b0dc4760fd3266e7bbfe0a800
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:34:15 -0300

    feat: Adding services dependency injection and Jwt configuration

--- MedicalCenterManagement/MedicalCenterManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -7,7 +7,43 @@ public static class ServiceCollectionExtensions
         var connectionString = configuration.GetConnectionString("DefaultConnection");
         services.AddDbContext<MedicalCenterManagementDbContext>(options => options.UseSqlServer(connectionString));
         services.AddScoped<IMedicalCenterManagementDbContext>(provider => provider.GetRequiredService<MedicalCenterManagementDbContext>());
+        services.AddJwt(configuration);
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
         
+        return services;
+    }
+
+    private static IServiceCollection AddJwt(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings");
+        services.Configure<JwtSettingsDto>(jwtSettings);
+
+        var appSettings = jwtSettings.Get<JwtSettingsDto>();
+        var key = Encoding.ASCII.GetBytes(appSettings.Key);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.Audience,
+                ValidIssuer = appSettings.Issuer
+            };
+        });
+
         return services;
     }
 }
