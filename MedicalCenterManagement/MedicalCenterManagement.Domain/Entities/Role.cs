e13a7210d9fd27ae2eadc93c0e76dd7fc2fb5ea8
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:47:35 -0300

    feat: Creating User and Role entities

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/Role.cs
@@ -0,0 +1,17 @@
+﻿namespace MedicalCenterManagement.Domain.Entities;
+
+public class Role : Entity
+{
+    public string Name { get; private set; }
+    public Guid UserId { get; private set; }
+    public User User { get; private set; }
+
+    private Role()
+    {
+    }
+
+    public Role(string name)
+    {
+        Name = name;
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace MedicalCenterManagement.Domain.Entities;
+
+public class User : Entity
+{
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public Guid RoleId { get; private set; }
+    public Role Role { get; private set; }
+    
+    public Doctor? Doctor { get; private set; }
+    public Patient? Patient { get; private set; }
+    
+    private User(){}
+    
+    public User(
+        string email, 
+        string password,
+        Role role
+    )
+    {
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+}
