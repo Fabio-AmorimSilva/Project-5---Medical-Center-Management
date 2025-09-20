ca3eaedde76f319973f5e99258548ee8ad4659e4
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:47:20 -0300

    feat: Creating User and Role configurations

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Persistence.Configurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder
+            .ToTable("Roles");
+        
+        builder
+            .HasKey(r => r.Id);
+
+        builder
+            .HasIndex(r => r.Name);
+        
+        builder
+            .Property(r => r.Name)
+            .IsRequired();
+        
+        builder
+            .HasOne(r => r.User)
+            .WithMany()
+            .HasForeignKey(r => r.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .ToTable("Users");
+
+        builder
+            .HasKey(u => u.Id);
+
+        builder
+            .HasIndex(u => u.Email);
+
+        builder
+            .Property(u => u.Email)
+            .IsRequired();
+
+        builder
+            .Property(u => u.Password)
+            .IsRequired();
+    }
+}
