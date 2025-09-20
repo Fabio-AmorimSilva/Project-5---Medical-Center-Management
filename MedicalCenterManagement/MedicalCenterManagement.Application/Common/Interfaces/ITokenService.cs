a802a2ab33332766f620dd5aa088c01f37807541
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:30:52 -0300

    feat: Creating TokenService

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Common/Interfaces/ITokenService.cs
@@ -0,0 +1,6 @@
+﻿namespace MedicalCenterManagement.Application.Common.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateToken(User user);
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Auth/Services/TokenService.cs
@@ -0,0 +1,28 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Auth.Services;
+
+public class TokenService(IOptions<JwtSettingsDto> options) : ITokenService
+{
+    private readonly JwtSettingsDto _jwtSettings = options.Value;
+
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
+        var claims = user.GetClaims();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.Expiration),
+            Audience = _jwtSettings.Audience,
+            Issuer = _jwtSettings.Issuer,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256
+            )
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
