9419d03f7b816c00ea9454df958ebeb2c208ded5
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:33:46 -0300

    feat: Creating JwtSettingsDto

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Auth/Dtos/JwtSettingsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Auth.Dtos;
+
+public sealed record JwtSettingsDto
+{
+    public string Key { get; init; } = null!;
+    public string Issuer { get; init; } = null!;
+    public string Audience { get; init; } = null!;
+    public int Expiration { get; init; }
+}
