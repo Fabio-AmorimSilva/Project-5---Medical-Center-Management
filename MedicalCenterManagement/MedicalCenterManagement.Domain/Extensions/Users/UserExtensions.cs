0df217dc5cff7a81bcd9bcb526712086f78581e5
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-20 17:48:45 -0300

    feat: Creating UserExtensions

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Extensions/Users/UserExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalCenterManagement.Domain.Entities;
+
+public static class UserExtensions
+{
+    public static IEnumerable<Claim> GetClaims(this User user)
+        => new List<Claim>
+        {
+            new(ClaimTypes.Sid, user.Id.ToString()),
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.Role, user.Role.Name)
+        };
+}
