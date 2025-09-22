cf0323646c08afb5c5b212462753ac1b312876f1
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:52:27 -0300

    feat: Creating new Specification for user email

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Specifications/UserEmailAlreadyExists.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalCenterManagement.Domain.Specifications;
+
+public sealed class UserEmailAlreadyExists : Specification<User>
+{
+    public UserEmailAlreadyExists(Guid userId, string email)
+    {
+        Query.Where(u =>
+            u.Id != userId &&
+            u.Email == email
+        );
+    }
+}
