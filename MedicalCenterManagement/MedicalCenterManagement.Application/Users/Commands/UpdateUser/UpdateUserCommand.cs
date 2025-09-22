880a44b1ce0c584a1e17dc7c77d12aa1704dedbf
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:50:41 -0300

    feat: Creating UpdateUserCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace MedicalCenterManagement.Application.Users.Commands.UpdateUser;
+
+public record UpdateUserCommand(
+    Guid UserId,
+    string Email,
+    Role Role,
+    ProfileType ProfileType
+) : IRequest<Response>;
+
+public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(command => command.UserId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.UserId)));
+
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.Email)))
+            .EmailAddress()
+            .WithMessage(ErrorMessages.InvalidEmail());
+
+        RuleFor(command => command.Role)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.Role)));
+
+        RuleFor(command => command.ProfileType)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.ProfileType)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.UpdateUser;
+
+public class UpdateUserCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<UpdateUserCommand, Response>
+{
+    public async Task<Response> Handle(UpdateUserCommand request)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == request.UserId);
+
+        if (user is null)
+            return new NotFoundResponse<User>(ErrorMessages.NotFound<User>());
+
+        user.Update(
+            email: request.Email,
+            role: request.Role,
+            profileType: request.ProfileType
+        );
+
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
