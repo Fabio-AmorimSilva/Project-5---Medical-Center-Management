8f3b19933b40910de4a52b603b2367ae080a97fe
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:51:59 -0300

    feat: Adding new enum Profile Type, Update and UpdatePassword methods

--- MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/User.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/Entities/User.cs
@@ -6,20 +6,40 @@ public class User : Entity
     public string Password { get; private set; }
     public Guid RoleId { get; private set; }
     public Role Role { get; private set; }
-    
+    public ProfileType ProfileType { get; private set; }
     public Doctor? Doctor { get; private set; }
     public Patient? Patient { get; private set; }
-    
-    private User(){}
-    
+
+    private User()
+    {
+    }
+
     public User(
-        string email, 
+        string email,
         string password,
-        Role role
+        Role role,
+        ProfileType profileType
     )
     {
         Email = email;
         Password = password;
         Role = role;
+        ProfileType = profileType;
+    }
+
+    public void Update(
+        string email,
+        Role role,
+        ProfileType profileType
+    )
+    {
+        Email = email;
+        Role = role;
+        ProfileType = profileType;
+    }
+
+    public void UpdatePassword(string password)
+    {
+        Password = password;
     }
 }
