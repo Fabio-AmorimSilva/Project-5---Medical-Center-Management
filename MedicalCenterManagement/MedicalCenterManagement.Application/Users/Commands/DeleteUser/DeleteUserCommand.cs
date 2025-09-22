8237b646f59969d55682f3cdfd93c9251a152a69
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:50:03 -0300

    feat: Creating DeleteUserCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.DeleteUser;
+
+public record DeleteUserCommand(Guid UserId) : IRequest<Response>;
+
+public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+{
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(command => command.UserId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteUserCommand.UserId)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.DeleteUser;
+
+public class DeleteUserCommandHandler(
+    IMedicalCenterManagementDbContext context
+) : IHandler<DeleteUserCommand, Response>
+{
+    public async Task<Response> Handle(DeleteUserCommand request)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == request.UserId);
+
+        if (user is null)
+            return new NotFoundResponse<User>(ErrorMessages.NotFound<User>());
+
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
