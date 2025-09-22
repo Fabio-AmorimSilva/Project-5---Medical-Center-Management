0b7e41d701a2acf1245568132b8db61ead868fd8
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:49:52 -0300

    feat: Creating CreateUserCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.CreateUser;
+
+public record CreateUserCommand(
+    string Email,
+    string Password,
+    Role Role,
+    ProfileType ProfileType
+) : IRequest<Response<Guid>>;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateUserCommand.Email)))
+            .EmailAddress()
+            .WithMessage(ErrorMessages.InvalidEmail());
+        
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateUserCommand.Password)));
+        
+        RuleFor(command => command.Role)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateUserCommand.Role)));
+        
+        RuleFor(command => command.ProfileType)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateUserCommand.ProfileType)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.CreateUser;
+
+public class CreateUserCommandHandler(
+    IMedicalCenterManagementDbContext context,
+    IPasswordHashService passwordHashService
+) : IHandler<CreateUserCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(CreateUserCommand request)
+    {
+        var passwordHash = passwordHashService.HashPassword(request.Password);
+
+        var user = new User(
+            email: request.Email,
+            password: passwordHash,
+            role: request.Role,
+            profileType: request.ProfileType
+        );
+
+        var emailAlreadyExists = await context.Users
+            .WithSpecification(new UserEmailAlreadyExists(user.Id, request.Email))
+            .AnyAsync();
+
+        if (emailAlreadyExists)
+            return new UnprocessableResponse<Guid>(ErrorMessages.MustBeUnique(nameof(request.Email)));
+
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+
+        return new CreatedResponse<Guid>(user.Id);
+    }
+}
