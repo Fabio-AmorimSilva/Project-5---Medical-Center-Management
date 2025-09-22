a394774747ddeb67024be05e20bbcce307b2663c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-22 19:50:55 -0300

    feat: Creating UpdatePasswordCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/UpdatePassword/UpdatePasswordCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.UpdatePassword;
+
+public record UpdatePasswordCommand(
+    Guid UserId,
+    string Password
+) : IRequest<Response>;
+
+public class UpdatePasswordCommandValidator : AbstractValidator<UpdatePasswordCommand>
+{
+    public UpdatePasswordCommandValidator()
+    {
+        RuleFor(command => command.UserId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdatePasswordCommand.UserId)));
+
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdatePasswordCommand.Password)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/UpdatePassword/UpdatePasswordCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.UpdatePassword;
+
+public class UpdatePasswordCommandHandler(
+    IMedicalCenterManagementDbContext context,
+    IPasswordHashService passwordHashService
+) : IHandler<UpdatePasswordCommand, Response>
+{
+    public async Task<Response> Handle(UpdatePasswordCommand request)
+    {
+        var user = await context.Users
+            .FirstOrDefaultAsync(u => u.Id == request.UserId);
+
+        if (user is null)
+            return new NotFoundResponse<User>(ErrorMessages.NotFound<User>());
+
+        var passwordHash = passwordHashService.HashPassword(request.Password);
+
+        user.UpdatePassword(password: passwordHash);
+
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
