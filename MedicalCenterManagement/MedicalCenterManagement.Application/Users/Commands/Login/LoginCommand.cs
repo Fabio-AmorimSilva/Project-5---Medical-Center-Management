2d8a808090591b9d563356de7139cd5227cefb9c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-23 19:45:40 -0300

    feat: Creating LoginCommand and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/Login/LoginCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.Login;
+
+public record LoginCommand(
+    string Email,
+    string Password
+) : IRequest<Response<string>>;
+
+public class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginCommand.Email)));
+
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginCommand.Password)));
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/Users/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace MedicalCenterManagement.Application.Users.Commands.Login;
+
+public class LoginCommandHandler(
+    IMedicalCenterManagementDbContext context,
+    IPasswordHashService passwordHashService,
+    ITokenService tokenService
+) : IHandler<LoginCommand, Response<string>>
+{
+    public async Task<Response<string>> Handle(LoginCommand request)
+    {
+        var passwordHash = passwordHashService.HashPassword(request.Password);
+
+        var user = await context.Users
+            .FirstOrDefaultAsync(u =>
+                u.Email == request.Email &&
+                u.Password == passwordHash
+            );
+
+        if (user is null)
+            return new NotFoundResponse<string>(ErrorMessages.NotFound<User>());
+
+        var token = tokenService.GenerateToken(user);
+
+        return new OkResponse<string>(token);
+    }
+}
