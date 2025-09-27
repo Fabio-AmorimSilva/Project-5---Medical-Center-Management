00c879d8e72d6093f950a19ab474a97eccf2e8f6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-09-27 18:33:19 -0300

    feat: Creating GoogleCalendarSettings

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Notifications/Google/Dtos/GoogleCalendarSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MedicalCenterManagement.Infrastructure.Notifications.Google.Dtos;
+
+public record GoogleCalendarSettings
+{
+    public string? ClientId { get; init; }
+    public string? ClientSecret { get; init; }
+    public string? ApplicationName { get; init; }
+}
