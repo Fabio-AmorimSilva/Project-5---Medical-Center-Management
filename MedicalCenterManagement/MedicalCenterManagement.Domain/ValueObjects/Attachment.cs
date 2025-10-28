449f0233a8719b98b43eea865adb4dfec19a1c33
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-10-28 20:03:03 -0300

    feat: Creating Attachment value object

---
+++ MedicalCenterManagement/MedicalCenterManagement.Domain/ValueObjects/Attachment.cs
@@ -0,0 +1,32 @@
+﻿namespace MedicalCenterManagement.Domain.ValueObjects;
+
+public class Attachment : ValueObject
+{
+    public string Name { get; private set; }
+    public string ContentType { get; private set; }
+    public long Size { get; private set; }
+    public DateTime UploadedAt { get; private set; }
+
+    private Attachment()
+    {
+    } 
+
+    public Attachment(
+        string name,
+        string contentType,
+        long size
+    )
+    {
+        Name = name;
+        ContentType = contentType;
+        Size = size;
+        UploadedAt = DateTime.UtcNow;
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Name;
+        yield return ContentType;
+        yield return Size;
+    }
+}
