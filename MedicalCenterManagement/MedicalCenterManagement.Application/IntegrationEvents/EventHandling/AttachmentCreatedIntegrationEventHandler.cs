f906dbd41d3b0889447f620abe18ac2f2714f338
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-10-28 19:57:47 -0300

    feat: Creating AttachmentCreatedIntegrationEvent.cs and handler

---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/IntegrationEvents/EventHandling/AttachmentCreatedIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicalCenterManagement.Application.IntegrationEvents.EventHandling;
+
+public class AttachmentCreatedIntegrationEventHandler(IFileStorageService fileStorageService)
+    : IIntegrationEventHandler<AttachmentCreatedIntegrationEvent>
+{
+    public async Task HandleAsync(AttachmentCreatedIntegrationEvent @event)
+    {
+        await fileStorageService.UploadAsync(
+            stream: @event.File,
+            fileName: @event.Name,
+            contentType: @event.ContentType
+        );
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Application/IntegrationEvents/Events/AttachmentCreatedIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace MedicalCenterManagement.Application.IntegrationEvents.Events;
+
+public record AttachmentCreatedIntegrationEvent : IntegrationEvent
+{
+    public Stream File { get; init; }
+    public string Name { get; init; } = null!;
+    public string ContentType { get; set; }
+
+    public AttachmentCreatedIntegrationEvent(
+        Stream file,
+        string name,
+        string contentType
+    )
+    {
+        File = file;
+        Name = name;
+        ContentType = contentType;
+    }
+}
