5a4c65bd31365d1694c8370461807f52620e8065
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-10-30 18:54:32 -0300

    feat: Creating new migration

--- MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Migrations/20251030215337_CreatingValueObjectAttachment.Designer.cs
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Migrations/20251030215337_CreatingValueObjectAttachment.Designer.cs
@@ -4,6 +4,7 @@ using MedicalCenterManagement.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicalCenterManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(MedicalCenterManagementDbContext))]
-    partial class MedicalCenterManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251030215337_CreatingValueObjectAttachment")]
+    partial class CreatingValueObjectAttachment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Migrations/20251030215337_CreatingValueObjectAttachment.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCenterManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingValueObjectAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileType",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Doctors_Attachments",
+                columns: table => new
+                {
+                    DoctorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<long>(type: "bigint", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors_Attachments", x => new { x.DoctorId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Doctors_Attachments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients_Attachments",
+                columns: table => new
+                {
+                    PatientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<long>(type: "bigint", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients_Attachments", x => new { x.PatientId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Patients_Attachments_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors_Attachments");
+
+            migrationBuilder.DropTable(
+                name: "Patients_Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileType",
+                table: "Users");
+        }
+    }
+}
