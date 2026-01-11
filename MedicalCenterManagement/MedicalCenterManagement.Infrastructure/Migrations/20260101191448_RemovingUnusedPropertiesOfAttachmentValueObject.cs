1fcdcc30be861c590a0d10d29fd93fc558922a32
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-01-11 17:48:32 -0300

    feat: Adding new migrations

---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Migrations/20260101191448_RemovingUnusedPropertiesOfAttachmentValueObject.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCenterManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingUnusedPropertiesOfAttachmentValueObject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Patients_Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Patients_Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Doctors_Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Doctors_Attachments");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Patients_Attachments",
+                newName: "Path");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Doctors_Attachments",
+                newName: "Path");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "Patients_Attachments",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "Doctors_Attachments",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Patients_Attachments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "Patients_Attachments",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Doctors_Attachments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "Doctors_Attachments",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
---
+++ MedicalCenterManagement/MedicalCenterManagement.Infrastructure/Migrations/20260103202720_UpdatingRoleToHaveAListOfUsers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCenterManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingRoleToHaveAListOfUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Roles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Roles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
